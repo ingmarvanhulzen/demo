c049dbe3fe23a62c243bc021575427bfb69e9e19
Ingmar van Hulzen <info@ivhdevelopment.com>
2024-02-22 22:29:21 +0100

    chore: add animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+import LetterJuggler from "@/components/LetterJuggler";
+
 export default function Home() {
   return (
     <main className="main">
       <div className="center">
-        <p>
+        <LetterJuggler>
           Sed augue ipsum, <strong>egestas</strong> nec, vestibulum et,
           malesuada adipiscing, dui. Mauris turpis nunc, blandit et, volutpat
           molestie, <u>porta ut</u>, ligula. Suspendisse feugiat. Maecenas
           egestas arcu quis ligula <em>mattis</em> placerat. Donec elit libero,
           sodales nec, volutpat a, suscipit non, turpis.
-        </p>
+        </LetterJuggler>
       </div>
     </main>
   );
