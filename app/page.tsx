6545c5cad1e7ccfba8c3da6d6392517cde342db5
Ingmar van Hulzen <info@ivhdevelopment.com>
2024-02-22 20:03:47 +0100

    chore: add more marks

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ export default function Home() {
     <main className="main">
       <div className="center">
         <p>
-          Sed augue ipsum, egestas nec, vestibulum et, malesuada adipiscing,
-          dui. Mauris turpis nunc, blandit et, volutpat molestie, porta ut,
-          ligula. Suspendisse feugiat. Maecenas egestas arcu quis ligula mattis
-          placerat. Donec elit libero, sodales nec, volutpat a, suscipit non,
-          turpis.
+          Sed augue ipsum, <strong>egestas</strong> nec, vestibulum et,
+          malesuada adipiscing, dui. Mauris turpis nunc, blandit et, volutpat
+          molestie, <u>porta ut</u>, ligula. Suspendisse feugiat. Maecenas
+          egestas arcu quis ligula <em>mattis</em> placerat. Donec elit libero,
+          sodales nec, volutpat a, suscipit non, turpis.
         </p>
       </div>
     </main>
